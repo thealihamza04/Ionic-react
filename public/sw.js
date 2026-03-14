34769a62e442af3e9ad60aca1eb39c1f222c1465
Ali Hamza <ah0681988@gmail.com>
2026-03-14 23:51:21 +0500

    Update timetable data and standalone timetable layout

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
